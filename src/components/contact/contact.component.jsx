26616625d0ab89d4c2fd8afc1579b79ab8c06f42
VandelayUtd <drewhadley.guitar@gmail.com>
2022-05-06 18:19:07 -0400

    added contact form

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -10,6 +10,28 @@ const Contact = () => {
             <div className='container contact-page'>
                 <div className='text-zone'>
                     <h1>Contact</h1>
+                    <p>shoot me your digits yo i wanna get paid</p>
+                    <div className='contact-form'>
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input type='text' name='name' placeholder='Name' required />
+                                </li>
+                                <li className='half'>
+                                    <input type='email' name='email' placeholder='Email' required />
+                                </li>
+                                <li>
+                                    <input placeholder='Subject' type='text' name='subject' required /> 
+                                </li>
+                                <li>
+                                    <textarea placeholder='Message' name='message' required ></textarea>
+                                </li>
+                                <li>
+                                    <input type='submit' className='flat-button' value='send' />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
             <Loader type='pacman' />

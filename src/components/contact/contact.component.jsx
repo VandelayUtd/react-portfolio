5045b747615fee6b805793a5dcd7ef7bc77be1d8
VandelayUtd <drewhadley.guitar@gmail.com>
2022-05-10 18:32:28 -0400

    setup email.js in contact compononent

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,9 +1,25 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import Loader from 'react-loaders';
 
 import './contact.styles.scss';
 
 
 const Contact = () => { 
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
 
     return (
         <>

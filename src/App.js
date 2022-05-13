0f04fe26a0214bd7b395dbd1074f9b58ad1d9692
VandelayUtd <drewhadley.guitar@gmail.com>
2022-05-13 11:44:31 -0400

    built projects component bones and added appropriate routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/about/about.component';
 
 import './App.scss';
 import Contact from './components/contact/contact.component';
+import Projects from './components/projects/projects.components';
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element={ <Layout /> } >
           <Route index element={ <Home /> } />
-          <Route path='about' element={ <About />} />
-          <Route path='contact' element={ <Contact/>} />
+          <Route path='about' element={ <About /> } />
+          <Route path='contact' element={ <Contact/> } />
+          <Route path='projects' element={ <Projects/> } />
         </Route>
       </Routes>
     </>
---
+++ src/components/projects/projects.components.jsx
@@ -0,0 +1,24 @@
+import './projects.styles.scss';
+
+const Projects = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href='https://imaginative-meerkat-6aa56a.netlify.app/' value='_blank' >Suburban Outfitters | Deployed</a>
+                </li>
+                <li>
+                    <a href='https://github.com/VandelayUtd/suburban-outfitters'>Suburban Outfitter | Repository</a>
+                </li>
+                <li>
+                    <a href='https://github.com/VandelayUtd/movie-phone'>Movie Phone | Repository</a>
+                </li>
+                <li>
+                    <a href='https://github.com/VandelayUtd/rotten-potatoes'>Rotten Potatos | Repository</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+export default Projects;

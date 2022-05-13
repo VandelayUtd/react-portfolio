de0941c0b5a5cf4ecfa2cc62bff0f639ae6e1c30
VandelayUtd <drewhadley.guitar@gmail.com>
2022-05-13 12:21:11 -0400

    links open in new tabs

--- src/components/projects/projects.components.jsx
+++ src/components/projects/projects.components.jsx
@@ -3,19 +3,20 @@ import './projects.styles.scss';
 const Projects = () => {
 
     return (
-        <div>
-            <ul>
-                <li>
-                    <a href='https://imaginative-meerkat-6aa56a.netlify.app/' value='_blank' >Suburban Outfitters | Deployed</a>
+        <div className='container projects-page'>
+            
+            <ul className='project-list'>
+                <li className='project-link'>
+                    <a href='https://imaginative-meerkat-6aa56a.netlify.app/' rel='noopener noreferrer'  target='_blank' >Suburban Outfitters | Deployed</a>
                 </li>
                 <li>
-                    <a href='https://github.com/VandelayUtd/suburban-outfitters'>Suburban Outfitter | Repository</a>
+                    <a href='https://github.com/VandelayUtd/suburban-outfitters'  rel='noopener noreferrer' target='_blank'>Suburban Outfitter | Repository</a>
                 </li>
                 <li>
-                    <a href='https://github.com/VandelayUtd/movie-phone'>Movie Phone | Repository</a>
+                    <a href='https://github.com/VandelayUtd/movie-phone' rel='noopener noreferrer' target='_blank'>Movie Phone | Repository</a>
                 </li>
                 <li>
-                    <a href='https://github.com/VandelayUtd/rotten-potatoes'>Rotten Potatos | Repository</a>
+                    <a href='https://github.com/VandelayUtd/rotten-potatoes' rel='noopener noreferrer' target='_blank'>Rotten Potatos | Repository</a>
                 </li>
             </ul>
         </div>

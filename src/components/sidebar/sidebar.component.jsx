2dfc79776290defcea6ffca056c4c9ac528c09ce
VandelayUtd <drewhadley.guitar@gmail.com>
2022-05-17 13:45:23 -0400

    added icon for projects route

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 import { ReactComponent as HomeIcon } from '../../assets/images/home-icon.svg'
 
@@ -23,6 +23,9 @@ const SideBar = () => {
                 </NavLink>
                 <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact' >
                     <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact='true' activeclassname='active' className='projects-link' to='/projects'>
+                    <FontAwesomeIcon icon={faLaptopCode} color='#4d4d4e' />
                 </NavLink>
             </nav>
             <ul>

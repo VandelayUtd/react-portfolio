5e777c339a70c2754436f2f731942c4ecc7407fd
VandelayUtd <drewhadley.guitar@gmail.com>
2022-06-14 15:27:34 -0400

    updated bio on about page

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -22,7 +22,7 @@ const About = () => {
                         About Me
                     </h1>
                     <p>I'm a Full Stack developer based out of Brooklyn, New York. Proficient in React, JavaScript, and Ruby on Rails with a zeal for creative problem solving. A problem is just progress that hasn't happened yet.</p>
-                    <p>I love to build things or break things and make them better. I have over a decade of experience in the music industry and when I'm not writing code I spend a lot of time working on vintage 50cc motorcycles, songs, cycling or hiking, and I'm self proclaimed film noir nerd. I approach building out an application with the same fervor and tenacity of building out a song, band, album, two stroke engine or following a trail of errors to fix a bug like Phillip Marlowe.</p>
+                    <p>I have over a decade of experience in the music industry as a touring and recording artist along with 2 years of fixing and rebuilding vintage 50cc mopeds, all of which has distilled strong values of collaboration and communication.</p>
                     <p>Code should be easily understood, efficient, fast, and designed with future modifications in mind.</p>
                 </div>
                 <div className="stage-cube-cont">

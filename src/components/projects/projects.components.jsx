d029c97d3f40c7851beb6c35b20a030d5a7b70a2
VandelayUtd <drewhadley.guitar@gmail.com>
2022-07-09 11:00:06 -0400

    added a button container for film fax buttons

--- src/components/projects/projects.components.jsx
+++ src/components/projects/projects.components.jsx
@@ -28,12 +28,15 @@ const Projects = () => {
                 <li>
                     <span>Film Fax</span>
                     <p>An application for cinefiles to get quick Film Facts and add or remove films from a watch list</p>
-                    <a className='flat-button' 
+                    <div className='button-container'>
                             href='https://exquisite-nasturtium-1de152.netlify.app/' 
                             rel='noopener noreferrer'  
                             target='_blank' 
                         >Deployed</a> 
-                    <a className='flat-button' href='https://github.com/VandelayUtd/movie-phone' rel='noopener noreferrer' target='_blank'>Repository</a>
+                    <a className='flat-button' href='https://github.com/VandelayUtd/film-fax' rel='noopener noreferrer' target='_blank'>Repository</a>
+
+                    </div>
+                    <a className='flat-button' 
                 </li>
                 <li>
                     <span>Rotten Potatoes</span>
